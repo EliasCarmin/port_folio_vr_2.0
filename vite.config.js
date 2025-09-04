ea6a919f21ff42948df4d62e092e1b233ab3c04b
EliasCarmin <130704962+EliasCarmin@users.noreply.github.com>
2025-09-04 10:17:22 -0500

    vite configuraciones

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/port_folio_vr_2.0/',
   server: {
     port: 3000,
     open: true
